4aaa717f19509f03223763639587a271f9206777
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-06-24 14:04:27 +0530

    Prototype Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Calculator")
+function handleCalculation(){
+    const principal = document.getElementById('principal').value;
+    const interest_rate = document.getElementById('interest_rate').value;
+    const time = document.getElementById('time').value;
+    const interest_element = document.getElementById('intrest');
+
+    const decimal_interest = parseFloat(interest_rate) / 100;
+
+    const interest = principal * decimal_interest * time; // I = P * R * T (Principal(P),Interest(I),Time(T))
+    interest_element.innerHTML = interest;
+}
