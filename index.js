6234d4c8463e022475bb66d74e4db483366c7f85
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-06-24 19:27:51 +0530

    Finising touch

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function handleCalculation(){
-    const principal = document.getElementById('principal').value;
-    const interest_rate = document.getElementById('interest_rate').value;
-    const time = document.getElementById('time').value;
-    const interest_element = document.getElementById('intrest');
+    const principal_amount = document.getElementById('principal').value;
+    const rate_of_interest = document.getElementById('interest_rate').value;
+    const allowed_time = document.getElementById('time').value;
+    const interest_container = document.getElementById('intrest');
+    const total_amount_container = document.getElementById('total_amount')
 
-    const decimal_interest = parseFloat(interest_rate) / 100;
+    const percent_to_decimal = parseFloat(rate_of_interest) / 100;
 
-    const interest = principal * decimal_interest * time; // I = P * R * T (Principal(P),Interest(I),Time(T))
-    interest_element.innerHTML = interest;
+    const interest = principal_amount * percent_to_decimal * allowed_time; // I = P * R * T (Principal(P),Interest of rate(R),Time(T))
+    const total_amount = parseFloat(principal_amount) + parseFloat(interest);
+
+    interest_container.innerHTML = (Math.round(interest * 100)/100).toFixed(2);
+    total_amount_container.innerHTML = (Math.round(total_amount * 100)/100).toFixed(2);
 }
